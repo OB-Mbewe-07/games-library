e4b81d580616140630cf47ef4488778aeb9d7929
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-04-20 14:18:45 +0200

    feat: folder structure, api fetch

---
+++ src/app/shared/models/data.model.ts
@@ -0,0 +1,10 @@
+export interface GameDealsData{
+    gameID: number;
+    salePrice: number;
+    normalPrice:  number;
+    title: string;
+    dealRating: number;
+    thump: string; //img
+    releaseDate: number;
+    storeID: number
+}
---
+++ src/app/shared/services/data.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { GameDealsData } from "../models/data.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataFetchService {
+    private http = inject(HttpClient);
+    private apiUrl = 'https://www.cheapshark.com/api/1.0/deals'
+
+    getDeals() {
+        return this.http.get<GameDealsData>(`${this.apiUrl}`);
+    }
+}
