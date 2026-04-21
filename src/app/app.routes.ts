569c9faeb7e7d73adee6d00bf9818b1fd1b282a2
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-04-21 10:00:58 +0200

    feat: add game details page prepped for ssr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { AvailableGamesComponent } from './components/available-games/available-games.component';
 import { LibraryComponent } from './components/library/library.component';
+import { GameDetailsComponent } from './components/game-details/game-details.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: AvailableGamesComponent
+        component: AvailableGamesComponent,
+        children: [{
+            path: ':id',
+            component: GameDetailsComponent
+        }]
     },
     {
         path: 'library',
